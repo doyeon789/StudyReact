cf87f0a7c682e2555a98ae8e00cefb76b3ce48bf
doyeon789 <ldyeon789@naver.com>
2026-03-10 15:53:07 +0900

    study node.js

---
+++ NodeJS/section3/src/index.js
@@ -0,0 +1,8 @@
+import randomColor from "randomcolor";
+import { add, sub } from "./math.js";
+
+const color = randomColor();
+console.log(color);
+
+console.log(add(1, 2));
+console.log(sub(1, 2));
